d9618754b49a4bb92380c702ef614d6b368cf22d
adz.macb003 <adz.macb003@MacBook-Air-de-adzmacb003.local>
2019-01-11 13:22:06 -0200

    navigate between components with queryParams and children routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,27 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    // this.server.id = this.router.params['id'];
+    const id = this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(1);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      )
+  }
+
+  onEdit() {
+    this.router.navigate(
+      ['edit'],
+      {relativeTo: this.route, queryParamsHandling: 'preserve'}
+    );
   }
 
 }
